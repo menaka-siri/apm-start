c9b132f22867cf4051c988e1d0db2fd6426bc1a4
menaka.siri <menaka.sirisena@eyepax.com>
2021-03-04 07:55:39 +0530

    implementing filter list functionality

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,7 +11,17 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number= 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+
+    private _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
           "productId": 1,
@@ -34,6 +44,17 @@ export class ProductListComponent implements OnInit{
           "imageUrl": "assets/images/garden_cart.png"
         },
     ];
+
+    constructor() {
+        this.filteredProducts = this.products;
+        //this.listFilter = 'cart';
+    }
+
+    performFilter(filterby: string): IProduct[] {
+        filterby = filterby.toLocaleLowerCase();
+        return this.products.filter( (product: IProduct)=>
+            product.productName.toLocaleLowerCase().indexOf(filterby) !== -1);
+    }
 
     toggleImage() : void{
         this.showImage = !this.showImage;
