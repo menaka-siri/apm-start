95c2bdc189bfe3dd74bb816b0e88fa300dc7aba4
menaka.siri <menaka.sirisena@eyepax.com>
2021-03-12 09:02:15 +0530

    registering an observable to get product list data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent} from './products/product-list.component';
 import { StarComponent } from './shared/star-rating/star.component'
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe'
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
